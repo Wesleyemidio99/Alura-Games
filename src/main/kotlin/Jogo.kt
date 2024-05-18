425808ce92dab029c0cb7f7d72961672b34a5761
wesley <wesleyemidio99@hotmail.com>
2024-05-18 20:17:50 -0300

    Recebendo os dados de uma api.

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,12 @@
+class Jogo(val titulo:String, val capa:String) {
+
+    val descricao = ""
+
+    override fun toString(): String {
+        return "Jogo:\n" +
+                "titulo: $titulo\n" +
+                "Capa: $capa\n" +
+                "Descricao: $descricao"
+    }
+
+}
