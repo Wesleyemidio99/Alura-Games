7120cba216704bb1e6496314fb4e86e85e616b95
wesley <wesleyemidio99@hotmail.com>
2024-05-18 21:30:35 -0300

    Iniciando uma variavel do tipo Jogo, e inserindo uma descricao nela

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -31,16 +31,30 @@ fun main() {
 //    }
 
 
+    var meuJogo:Jogo? = null
+
     // Caso o id seja encontrado, é guardado um secesso dentro da variavel
     val resultado = runCatching {
         val meuInfoJogo = gson.fromJson(json,InfoJogo::class.java)
-        val meuJogo = Jogo(meuInfoJogo.info.title,meuInfoJogo.info.thumb)
+        meuJogo = Jogo(meuInfoJogo.info.title,meuInfoJogo.info.thumb)
         println(meuJogo)
     }
 
     //Verifica se houve falha dentro de resultado
     resultado.onFailure {
         println("Jogo inexistente, tente outro id.")
+    }
+
+    resultado.onSuccess {
+        println("Deseja inserir uma descrição ao jogo? S/N")
+        val opcao = leitura.nextLine()
+        if (opcao.equals("s",true)) {
+            println("Digite a descricao do jogo: ")
+            val descricaoPersonalizado = leitura.nextLine()
+            meuJogo?.descricao = descricaoPersonalizado
+        }else{
+            meuJogo?.descricao = meuJogo?.titulo
+        }
     }
 
 }
